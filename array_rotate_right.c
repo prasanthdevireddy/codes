376efdf6a359253300774d20f4db4f8c8a592f1d
Prasanth <prasanthdevireddy@gmail.com>
2017-07-20 22:29:05 -0700

    array rotate initial commit

---
+++ array_rotate_right.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+void array_rotate_right(int array[]);
+int main(){
+
+	int array[10];
+	int i,k;
+
+	printf("Enter the array Elements\n");
+	for(i=0;i<10;i++){
+	//printf("Enter the array elements\n");
+	scanf("%d\n",&array[i]);
+	}
+   
+   //     array_rotate_right(array);
+	
+	for(k=0;k<=10;k++)
+		array[k+1] = array[k];
+		printf("rotated values are %d\n", array[k+1]);
+	}
+	/*for(j=0;j<10;j++){
+	printf("rotated array is %d\n", array[j]);
+	}*/
+
+
+/*void array_rotate_right(int array []){
+
+	int k,l;
+
+	for(k=0;k<=10;k++){
+		array[k+1] = array[k];
+		printf("value of K is %d,%d\n", array[k],array[k+1]);
+	}
+}*/
+		 
